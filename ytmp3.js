29e23074f2affff9a3353c27bf50672982173af9
DINU-BACKUP <hikunaherokuandgithub@gmail.com>
2025-11-23 18:41:09 +0530

    Delete ytmp3.js

--- ytmp3.js
+++
@@ -1,8 +0,0 @@
-import { ytmp3 } from "../youtube-scraper";
-
-export default async function handler(req, res) {
-	const { url, quality } = req.query;
-	if (!url) return res.status(400).json({ status: false, message: "URL required!" });
-	const result = await ytmp3(url, Number(quality) || 128);
-	res.status(200).json(result);
-}
